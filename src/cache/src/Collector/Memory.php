78a2b66a6ea8cdce7a02696ef33116f307890b6f
NoraClapham <noraclapham@stu.mju.edu.cn>
2026-01-29 19:21:32 +0800

    Update Memory.php
    修复内存驱动 cache()->set 未设置ttl 无法使用默认TTL的bug

--- src/cache/src/Collector/Memory.php
+++ src/cache/src/Collector/Memory.php
@@ -50,7 +50,7 @@ final class Memory
 
     public function has(string $key): bool
     {
-        return isset($this->keys[$key]);
+        return array_key_exists($this->keys[$key]);
     }
 
     public function get(string $key, mixed $default = null): mixed
